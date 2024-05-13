1c6163ddfcd65c3516712e2aa8e6cf03a4ebc92e
Solace2K <149768584+Solace2K@users.noreply.github.com>
2024-05-12 20:32:20 -0500

    12346
    blah

--- backend/index.js
+++ backend/index.js
@@ -12,7 +12,7 @@ const cors = require("cors");
 const app = express();
 
 const jwt = require("jsonwebtoken");
-const { authenticateToken} = require("./utilities");
+const { authenticateToken } = require("./utilities");
 
 app.use(express.json());
 
