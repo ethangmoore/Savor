3606992f929d89cc56225488eb9c0d2fbb5af8b9
Solace2K <149768584+Solace2K@users.noreply.github.com>
2024-05-19 14:22:33 -0500

    Login API intergration

--- frontend/recipe-app/src/pages/Login/Login.jsx
+++ frontend/recipe-app/src/pages/Login/Login.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import Navbar from "../../components/Navbar/Navbar";
-import { Link } from "react-router-dom"; 
+import { Link, useNavigate } from "react-router-dom"; 
 import PasswordInput from '../../components/Input/PasswordInput';
 import { validateEmail } from '../../utils/helper';
 
@@ -9,6 +9,8 @@ const Login = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [error, setError] = useState(null);
+
+  const navigate = useNavigate()
 
   const handleLogin = async (e) => {
     e.preventDefault();
@@ -25,6 +27,26 @@ const Login = () => {
 
     setError()
 
+    // Login API Call
+    try {
+        const response = await anxiosInstance.post("/login", {
+            email: email,
+            password: password,
+        });
+
+    // Handle Successful Login Response
+    if (response.data && response.data.accessToken){
+        localStorage.setItem("token", response.data.accessToken)
+        navigate('/dashboard')
+    }
+    } catch {error} {
+        // Handle Login Error
+        if (error.response && error.response.data && error.response.data.message) {
+            setError(error.response.data.message);
+        } else {
+            setError(" An Unexpected error has occured. Please try again.");
+        }
+    }
 
   };
 
